52811d2fefb5bec9e677c1a55410bbabbb459fec
Moses Williams III <varenigmatictheprogrammer1@gmail.com>
2016-08-05 17:52:08 -0400

    fix
    fix for quiz questions

--- objects/js/shorter-object.js
+++ objects/js/shorter-object.js
@@ -1,5 +1,5 @@
 var questions = [
-  { question: 'How many states are in the United States?', answer: 50 },
-  { question: 'How many continents are there?', answer: 7 },
-  { question: 'How many legs does an insect have?', answer: 6 }
+  { question: 'What is my age?', answer: 30 },
+  { question: 'How many letters are in my name?', answer: 5 },
+  { question: 'How many inches are in a foot?', answer: 12 }
 ];
